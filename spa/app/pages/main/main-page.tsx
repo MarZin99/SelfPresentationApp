12dd2ac08445a2555a73267dfa6bd8c1394f0d4a
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-04-09 22:59:30 +0200

    Remove icon from MainPage

--- spa/app/pages/main/main-page.tsx
+++ spa/app/pages/main/main-page.tsx
@@ -1,10 +1,8 @@
-import PageIcon from '@/app/assets/icons/logo'
 import '../../globals.css'
 
 export default function MainPage() {
     return (
         <div className="main-background">
-            <PageIcon/>
         </div>
     )
  }

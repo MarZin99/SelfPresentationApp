ed40e9335fecc2d37b745f62a1f3c54cf096f03a
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-04-09 23:31:54 +0200

    Add Card template and placing in MainPage

---
+++ spa/app/components/Card/card.tsx
@@ -0,0 +1,9 @@
+import { CardProps } from "./card-props";
+
+export default function Card({}: CardProps) {
+  return (
+    <div className="card p-2">
+      <div className="grid">asdasd</div>
+    </div>
+  );
+}
--- spa/app/pages/main/main-page.tsx
+++ spa/app/pages/main/main-page.tsx
@@ -1,10 +1,15 @@
-import PageIcon from '@/app/assets/icons/logo'
-import '../../globals.css'
+import "../../globals.css";
+import Card from "@/app/components/Card/card";
 
 export default function MainPage() {
-    return (
-        <div className="main-background">
-            <PageIcon/>
-        </div>
-    )
- }
+  return (
+    <div className="main-background justify-center items-center">
+      <div
+        className="flex justify-center items-center bg-red-500"
+        style={{ height: "800px", width: "500px" }}
+      >
+        <Card />
+      </div>
+    </div>
+  );
+}
